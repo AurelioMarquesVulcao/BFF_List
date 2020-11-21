4333e9c373ea23267f406e99e07832d21e1425ea
Vulcao <amarques@impacta.adv.br>
2020-11-21 14:55:43 -0300

    Salvando componentes iniciais

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from 'react';
+import Routes from './routes';
 import './assets/bootstrap-4.1.3/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+
+// import logo from './logo.svg';
+// import './App.css';
+// import { Button } from 'react-bootstrap';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <div class="">
 
-      <div class="row alert alert-primary justify-content-between" role="alert">
-        Teste de um belo card
-        <Button variant="primary">Primary</Button>{' '}
+// function App() {
+//   return (
+//     <div className="App">
+//       <div class="">
+
+//       <div class="row alert alert-primary justify-content-between" role="alert">
+//         Teste de um belo card
+//         <Button variant="primary">Primary</Button>{' '}
+//       </div>
+
+//       </div>
+
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Routes />
       </div>
-
-      </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import Lista from '../../monitorias/app/src/pages/Monitoria01'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      {/* <Route exact path="/01" component={Lista} /> */}
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
