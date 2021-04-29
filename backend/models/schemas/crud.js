6e8d50190d841c29aaaf0135703cf3817a4ff46d
AurelioVulcao <aurelio.vulcao@olist.com.br>
2021-04-29 11:09:10 -0300

    Data base Expansion

--- backend/models/schemas/crud.js
+++ backend/models/schemas/crud.js
@@ -1,13 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dataBase = new mongoose.Schema({
-    // Date: Date,
-    productName: String,
-    sellPrice:Number,
-    image: String
+  creatDate: Date,
+  productName: String,
+  sellPrice: Number,
+  shopSellPrice: [{ shopName: String, shopId: String, sellPrice: Number }],
+  image: String,
+});
 
-})
-
-const DataProduct = mongoose.model('DataProduct', dataBase, 'dataProduct');
+const DataProduct = mongoose.model("DataProduct", dataBase, "dataProduct");
 
 module.exports.DataProduct = DataProduct;
+
+const dataMarketplace = new mongoose.Schema({
+  creatDate: Date,
+  marketplaceName: String,
+  image: String,
+});
+
+const DataMarketplace = mongoose.model("DataMarketplace", dataMarketplace, "DataMarketplace");
+
+module.exports.DataMarketplace = DataMarketplace;
+
+const shoppingList = new mongoose.Schema({
+  Date: Date,
+  shopName: String,
+  productName: String,
+  sellPrice: Number,
+  image: String,
+});
+
+const ShoppingList = mongoose.model("ShoppingList", shoppingList, "shoppingList");
+
+module.exports.DataProduct = ShoppingList;
