17c4b267fe27047678278a4c71248ac188416cc7
AurelioVulcao <aurelio.vulcao@olist.com.br>
2021-04-30 11:59:16 -0300

    install base dependencies

--- frontend/server.js
+++ frontend/server.js
@@ -12,6 +12,4 @@ app.get('*', (req,res) => res.sendFile('index.html' , { root : baseDir }))
 
 const port = 3000
 
-
-
 app.listen(port, () => console.log(`Servidor subiu com sucesso em http://localhost:${port}`))
