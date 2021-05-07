e4434b61ee474e619c4097a276d90c504c1745cb
kelvin diego <39449648+diegofullstackjs@users.noreply.github.com>
2021-05-07 10:49:10 -0300

    Update index.tsx
    Nao sei se funcionaq

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,15 +1,27 @@
 import { Button, Flex, Heading, Input } from "@chakra-ui/react";
 // import React from "react";
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 
 const LoginPage = () => {
+  const history = useHistory
+  const api = () => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+          reoslve("OK")
+    },5000);
+  }
+  const handleSubmit = () => {
+      api().then((resolve) => {
+          history.push("/home")
+      })
+    }
   return(
   <Flex height="100vh" alignItems="center" justifyContent="center">
     <Flex direction="column" background="red.100" p={12} rounded={6}>
       <Heading mb={6} align="center"> Bem Vindo!</Heading>
       <Input placeholder="aurelio@teste.com" variant="filled" mb={3} type="email" />
       <Input placeholder="*****" variant="filled" mb={6} type="password" />
-      <Button colorScheme="teal" href="home">
+      <Button colorScheme="teal" onClick={() => handleSubmit}>
         Log in
       </Button>
     </Flex>
