2bb452e6577eccf68897c0e286ace32c084bbf22
AurelioVulcao <aurelio.vulcao@olist.com.br>
2021-05-12 10:30:49 -0300

    Testing minimum test configurations

---
+++ frontend/src/tests/App.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+// import Home from '../pages/home';
+import LoginPage from '../pages/login';
+
+test('renders App', () => {
+  render(<App />);
+
+  const linkElement = screen.getByText("Log in");
+
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Login', () => {
+  render(<LoginPage />);
+
+  const linkElement = screen.getByText("Log in");
+
+  expect(linkElement).toBeInTheDocument();
+});
