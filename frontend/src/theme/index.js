ec13a190e84f11adafdbf54e1c2cd41d06cb63fc
AurelioVulcao <aurelio.vulcao@olist.com.br>
2021-05-26 17:26:01 -0300

    Home Contruction

--- frontend/src/theme/index.js
+++ frontend/src/theme/index.js
@@ -1,7 +1,10 @@
-import {extendTheme} from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
+// import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const theme = extendTheme({
-    
-})
+
+// const breakpoints = ["0em", "30em", "48em", "62em", "80em", "96em"];
+
+const theme = extendTheme({});
 
 export default theme;
+
